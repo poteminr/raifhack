88757ef22e6d11636230ce58eee1e5d857ce340b
Roman Potemin <poteminr@gmail.com>
2021-09-25 11:03:02 +0700

    add statistics

--- features.py
+++ features.py
@@ -23,6 +23,39 @@ def haversine(lon1, lat1, lon2, lat2):
     km = 6371* c
     return km
 
+def calculate_statistic(dataframe):
+    dataframe['population_per_house_1000'] = dataframe['reform_house_population_1000'] / dataframe['reform_count_of_houses_1000']
+    dataframe['population_per_house_500'] = dataframe['reform_house_population_500'] / dataframe['reform_count_of_houses_500']
+
+    dataframe['population_per_floor_1000'] = dataframe['population_per_house_1000'] / dataframe['reform_mean_floor_count_1000']
+    dataframe['population_per_floor_500'] = dataframe['population_per_house_500'] / dataframe['reform_mean_floor_count_500']
+
+    dataframe['mean_house_age_500'] = 2020 - dataframe['reform_mean_year_building_500']
+    dataframe['mean_house_age_1000'] = 2020 - dataframe['reform_mean_year_building_1000']
+
+
+    radius_buildings_features = ['osm_catering_points_in_', 'osm_shops_points_in_', 'osm_offices_points_in_', 'osm_finance_points_in_', 
+                       'osm_healthcare_points_in_', 'osm_leisure_points_in_', 'osm_hotels_points_in_', 'osm_amenity_points_in_']
+    
+    population_features = radius_buildings_features + ['osm_leisure_points_in_', 'osm_culture_points_in_', 'osm_train_stop_points_in_',
+                                                       'osm_transport_stop_points_in_', 'osm_crossing_points_in_']
+
+    col_list = dataframe.columns
+    # for radius in ['0.001', '0.005', '0.0075', '0.01']:
+    #     for feature in radius_buildings_features:
+    #         col_name = feature + radius
+    #         if col_name in col_list:
+    #             dataframe[f'percent of {col_name}'] = dataframe[col_name] / dataframe[f'osm_building_points_in_{radius}']
+
+    for radius in ['0.005', '0.01']:
+        for feature in population_features:
+            col_name = feature + radius
+            if col_name in col_list:
+
+                dataframe[f'{col_name} per population'] = dataframe[col_name] / dataframe[f'reform_house_population_{int(float(radius)*100000)}'] 
+
+    return dataframe
+
 
 def preproces_data(dataframe):
     dataframe = pd.merge(dataframe, salary_data, on='region')
